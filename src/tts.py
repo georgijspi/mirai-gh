53b9edf4eea78b7278cfaccf20c3119bb282a3a3
Georgijs Pitkevics <georgijs.pitkevics2@mail.dcu.ie>
2024-11-12 02:12:08 +0000

    Simple XTTS-2 test.

---
+++ src/tts.py
@@ -0,0 +1,23 @@
+import torch
+from TTS.api import TTS
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Define paths to your data and config files
+wav_file_path = "voicelines/cleaned/rifleman_clean.wav"  # Path to your wav file
+output_audio_path = "output"        # Directory where model will be saved
+config_path = "config.json"          # Coqui TTS config file
+
+tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
+
+def generate_audio(
+    text="A long time ago in a galaxy far, far away. A child was born of the force."):
+    tts.tts_to_file(
+        text="I'm a barbie girl, unequivocally in a barbie world. Life, it is, in plastic, and surely, it's fantastic.",
+        file_path="output2.wav",
+        speaker_wav="voicelines/cleaned/morgan_cleaned.wav",
+        language="en")
+    return "output2.wav"
+
+print(generate_audio())
+
